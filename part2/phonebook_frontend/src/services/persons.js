f9ccd520add135b2311181b117532d77bd343b70
rmarjora <rasmus.marjoranta@gmail.com>
2025-06-06 13:17:50 +0300

    fixed port in services

--- part2/phonebook_frontend/src/services/persons.js
+++ part2/phonebook_frontend/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const PORT = process.env.PORT || 3001
+const baseUrl = `http://localhost:${PORT}/api/persons`
 
 const getAll = () => {
   const request = axios.get(baseUrl)
