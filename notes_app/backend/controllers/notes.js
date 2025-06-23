cfae4d35dcfdc50dd981983c24622021da1c5c1d
rmarjora <rasmus.marjoranta@gmail.com>
2025-06-23 11:28:12 +0300

    notes app token auth

--- notes_app/backend/controllers/notes.js
+++ notes_app/backend/controllers/notes.js
@@ -1,5 +1,7 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -15,15 +17,35 @@ notesRouter.get('/:id', async (request, response) => {
   }
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const payload = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!payload.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(payload.id)
+
+  if(!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
