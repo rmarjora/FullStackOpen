cf7d904232eaf3d6c31e815253e287bc4eea2c9f
rmarjora <rasmus.marjoranta@gmail.com>
2025-06-24 10:41:11 +0300

    add tests for user creation

--- part4/blog-list/tests/bloglist_api.test.js
+++ part4/blog-list/tests/bloglist_api.test.js
@@ -1,62 +1,26 @@
 const assert = require('node:assert')
-const { test, after, beforeEach } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const helper = require('./helper')
 
 const api = supertest(app)
 
-const initialBlogs = [
-    {
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7
-    },
-    {
-        title: "Go To Statement Considered Harmful",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-        likes: 5
-    },
-    {
-        title: "Canonical string reduction",
-        author: "Edsger W. Dijkstra",
-        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-        likes: 12
-    },
-    {
-        title: "First class tests",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-        likes: 10
-    },
-    {
-        title: "TDD harms architecture",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-        likes: 0
-    },
-    {
-        title: "Type wars",
-        author: "Robert C. Martin",
-        url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-        likes: 2
-    }
-]
-
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(initialBlogs)
+    await helper.initializeDB()
 })
 
 test('All notes are returned in JSON format', async () => {
+    const blogsAtStart = await helper.getAllBlogs()
     const response = await api
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
-    assert.strictEqual(response.body.length, initialBlogs.length)
+    assert.strictEqual(response.body.length, blogsAtStart.length)
 })
 
 test('identifier property is named \'id\'', async () => {
@@ -65,6 +29,7 @@ test('identifier property is named \'id\'', async () => {
 })
 
 test('A blog can be created via HTTP POST request', async () => {
+    const blogsAtStart = await helper.getAllBlogs()
     const testBlog = {
         title: "Test",
         author: "Test Author",
@@ -74,7 +39,7 @@ test('A blog can be created via HTTP POST request', async () => {
     await api.post('/api/blogs').send(testBlog).expect(201).expect('Content-Type', /application\/json/)
 
     const allBlogs = await Blog.find({})
-    assert.strictEqual(allBlogs.length, initialBlogs.length + 1)
+    assert.strictEqual(allBlogs.length, blogsAtStart.length + 1)
 })
 
 test('Blog with missing likes defaults to 0 likes', async () => {
@@ -84,7 +49,7 @@ test('Blog with missing likes defaults to 0 likes', async () => {
         url: "test.test"
     }
     
-    await Blog.insertOne(testBlog)
+    await Blog.create(testBlog)
 
     const noLikesBlog = await Blog.findOne(testBlog)
     assert.strictEqual(noLikesBlog.likes, 0)
@@ -99,7 +64,7 @@ test('Respond with 400 Bad Request when Blog is missing title or url', async () 
         },
         {
             title: "No url",
-            autor: "No URL Author",
+            author: "No URL Author",
             likes: 0
         }
     ]
@@ -113,16 +78,53 @@ test('Respond with 400 Bad Request when Blog is missing title or url', async () 
 })
 
 test('A blog can be deleted', async () => {
+    const blogsAtStart = await helper.getAllBlogs()
     const blog = await Blog.findOne({})
     const idToDelete = blog.id
     await api.delete(`/api/blogs/${idToDelete}`).expect(204)
 
-    const allBlogs = await Blog.find({})
-    assert.strictEqual(allBlogs.length, initialBlogs.length - 1)
+    const blogsAtEnd = await Blog.find({})
+    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1)
 })
 
 test('The likes of a blog can be updated', async () => {
     
+})
+
+describe('User tests', () => {
+  test('Creating non-unique username fails', async () => {
+    const usersAtStart = await helper.getAllUsers()
+    const newUser = {
+      username: 'testuser',
+      name: 'Test User',
+        password: 'testpassword'
+    }
+    const result = await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
+    assert(result.body.error.includes('expected `username` to be unique'))
+    const usersAtEnd = await helper.getAllUsers()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+    })
+    
+  test('User creation fails when password is too short', async () => {
+    const usersAtStart = await helper.getAllUsers()
+    const newUser = {
+      username: 'us',
+      name: 'Test User',
+      password: 'pw'
+    }
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+    assert(result.body.error.includes('Password length must be at least 3 characters'))
+    const usersAtEnd = await helper.getAllUsers()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
+ })
 })
 
 after(async () => {
